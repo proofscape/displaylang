e46dbcdf98bae8c4f9ddb281da7d2c2bd3131e68
Steve Kieffer <sk@skieffer.info>
2023-01-15 14:06:46 -0500

    Update attributions

--- displaylang/allow.py
+++ displaylang/allow.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------------------- #
 #   DisplayLang                                                               #
 #                                                                             #
-#   Copyright (c) 2020-2022 DisplayLang contributors                          #
+#   Copyright (c) 2020-2023 DisplayLang Contributors                          #
 #                                                                             #
 #   Licensed under the Apache License, Version 2.0 (the "License");           #
 #   you may not use this file except in compliance with the License.          #
